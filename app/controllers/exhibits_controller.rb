dd0f5c39b8e334304d3cf6b6ccca2e4b39d61b4f
Maximilian Wagner <maw3350@gmail.com>
2022-02-04 08:35:35 -0500

    feat:user story 3 complete!

---
+++ app/controllers/exhibits_controller.rb
@@ -0,0 +1,10 @@
+class ExhibitsController < ApplicationController
+  def index
+    @exhibits = Exhibit.all
+  end
+
+  def show
+    @exhibit = Exhibit.find(params[:id])
+  end
+
+end
--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -1,4 +1,8 @@
 class Exhibit < ApplicationRecord
   belongs_to :zoo
 
+  def zoo_name
+    zoo.name
+  end
+
 end
