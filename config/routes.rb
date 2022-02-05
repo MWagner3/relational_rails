f0841ddca4447ff1caac5d8b59d1039a7514868b
Maximilian Wagner <maw3350@gmail.com>
2022-02-05 11:32:57 -0500

    Add links to each index on every page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/zoos', to: 'zoos#index'
+  get '/zoos/:id', to: 'zoos#show'
   get '/exhibits', to: 'exhibits#index'
+  get '/exhibits/:id', to: 'exhibits#show'
 end
